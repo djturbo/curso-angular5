d4b0be3b2922a3c6e9408fb088f1710b57772de2
francisco jose <fjarquellada@vectoritcgroup.com>
2018-01-15 19:29:19 +0100

    desabilitando clustering en log4js configuracion

--- log4js.config.js
+++ log4js.config.js
@@ -1,8 +1,10 @@
 const log4js = require('log4js');
 log4js.configure({
 	appenders: { zoo: { type: 'file', filename: 'zoo.log' } },
-	categories: { default: { appenders: ['zoo'], level: 'error' } }
-  });
+	categories: { default: { appenders: ['zoo'], level: 'error' } },
+	disableClustering: true
+});
    
 const logger = log4js.getLogger('zoo');
+logger.level = 'debug';
 module.exports = logger;
